4f9064399cf33865aaa5d82eab1c5f67ca664e43
Ghanshayam Kumar Giet <ghanshyam.kumar.edu@gmail.com>
2024-10-17 22:08:59 +0530

    test

---
+++ Server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h1>  Welcome to our  E commerce app </h1>");
+});
+
+const PORT = process.env.PORT || 5500;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
